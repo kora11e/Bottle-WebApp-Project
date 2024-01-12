899af0426b5852b97b9031b865435595fb737743
kora11e <87006301+kora11e@users.noreply.github.com>
2024-01-12 16:36:10 +0100

    update

---
+++ alchemy.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Person(Base):
+    __tablename__ = 'persons'
+
+    ssn = Column('ssn', Integer, primary_key = True)
+    furstname = []
